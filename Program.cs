974ef5434c0770dc7ade7c1aa9c32adb62879814
Gubariev <rostislav.gubarev.r1wor@gmail.com>
2021-04-19 21:55:49 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using AngleSharp;
+using AngleSharp.Extensions;
+using System;
+using System.Threading.Tasks;
+
+namespace RequestsTest
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            var TestLinks = new LinksReader();
+            TestLinks.WebAddress = Console.ReadLine();
+            var xmlLinks = TestLinks.XmlReadLinks();
+            var htmlLinks = TestLinks.HtmlReadLinks();
+
+            TestLinks.Compare(htmlLinks.Result, xmlLinks.Result);
+            TestLinks.ElapseTime(htmlLinks.Result);
+
+            Console.ReadKey();
+        }
+    }
+}
